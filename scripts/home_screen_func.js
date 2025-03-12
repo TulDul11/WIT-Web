df09b72ccfa88106aad5275ae34b051a0e34feb2
TulDul11 <dinichen1108@gmail.com>
2025-03-12 14:13:03 -0600

    Side bar functionality added

--- scripts/home_screen_func.js
+++ scripts/home_screen_func.js
@@ -5,6 +5,29 @@ window.addEventListener('DOMContentLoaded', function() {
     const user = userInfo.user;
 
     if (user) {
-        greeting_text.textContent = "Bienvenido, " + user; 
+        greeting_text.textContent = user; 
     }
 });
+
+function openSideMenu() {
+    const side_menu = document.getElementById('side_menu');
+    const bg_overlay = document.getElementById('background_overlay');
+    side_menu.classList.add('active');
+    bg_overlay.classList.add('active')
+}
+
+function closeSideMenu() {
+    const side_menu = document.getElementById('side_menu');
+    const bg_overlay = document.getElementById('background_overlay');
+    side_menu.classList.remove('active');
+    bg_overlay.classList.remove('active');
+}
+
+function homeScreen() {
+    window.location.href = `../templates/home.html`;
+}
+
+function logOut() {
+    sessionStorage.removeItem('userInfo');
+    window.location.href = `../templates/index.html`;
+}

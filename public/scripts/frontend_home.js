c2459161be4aace4a493358360cf969fd62cb591
TulDul11 <dinichen1108@gmail.com>
2025-04-04 00:45:30 -0600

    Navigation bar finished

--- public/scripts/frontend_home.js
+++ public/scripts/frontend_home.js
@@ -1,19 +1,22 @@
 window.addEventListener('load', (event) => {
-    const welcome_text = document.getElementById('greeting_text');
+    const user_role_text = document.getElementById('nav_role');
 
     user_info = JSON.parse(sessionStorage.getItem('user_info'));
     user_id = user_info.user_id;
+    let user_role;
 
     if (user_info.user_role == 'alumno') {
         /* -> Deployment */
         //fetch_url = `http://pk8ksokco8soo8ws0ks040s8.172.200.210.83.sslip.io/user_home/${user_id}/alumnos`
         /* -> Local development */
         fetch_url = `http://localhost:3000/user_home/${user_id}/alumnos`;
+        user_role = 'Alumno';
     } else if (user_info.user_role == 'profesor') {
         /* -> Deployment */
         //fetch_url = `http://pk8ksokco8soo8ws0ks040s8.172.200.210.83.sslip.io/user_home/${user_id}/profesores`
         /* -> Local development */
         fetch_url = `http://localhost:3000/user_home/${user_id}/profesores`;
+        user_role = 'Profesor';
     }
     fetch(fetch_url)
             .then(response => {
@@ -24,9 +27,9 @@ window.addEventListener('load', (event) => {
             })
             .then(data => {
                 if (data.apellido) {
-                    welcome_text.textContent = "Hola, " + data.nombre + ' ' + data.apellido;
+                    user_role_text.textContent = user_role + ' ' + data.nombre + ' ' + data.apellido;
                 } else {
-                    welcome_text.textContent = "Hola, " + data.nombre;
+                    user_role_text.textContent = user_role + ' ' + data.nombre;
                 }
             })
             .catch(error =>
--- public/scripts/frontend_login.js
+++ public/scripts/frontend_login.js
@@ -2,24 +2,32 @@ function login_auth() {
     const user_id = document.getElementById('login_user').value;
     const user_password = document.getElementById('login_password').value;
 
-    const error_text = document.getElementById('error_message');
+    const error_text = document.getElementById('error_text');
+    const error_symbol = document.getElementById('error_symbol');
 
     if (!user_id && !user_password) {
-        error_text.textContent = 'Error: Se requiere llenar los campos de usuario y contraseña.'
+        error_text.textContent = 'Se requiere llenar los campos de usuario y contraseña.';
+        error_symbol.style.display = 'grid';
     } else if (!user_id) {
-        error_text.textContent = 'Error: Se requiere llenar el campo de usuario.'
+        error_text.textContent = 'Se requiere llenar el campo de usuario.';
+        error_symbol.style.display = 'grid';
     } else if (!user_password) {
-        error_text.textContent = 'Error: Se requiere llenar el campo de contraseña.'
+        error_text.textContent = 'Se requiere llenar el campo de contraseña.';
+        error_symbol.style.display = 'grid';
     } else {
+        error_text.textContent = '';
+        error_symbol.style.display = 'none';
         /* -> Deployment */
         /*
         fetch(`http://pk8ksokco8soo8ws0ks040s8.172.200.210.83.sslip.io/login_info/${user_id}`)
             .then(response => {
                 if (!response.ok) {
                     if (response.status === 404) {
-                        error_text.textContent = 'Error: Usuario no encontrado';
+                        error_text.textContent = 'Usuario no encontrado';
+                        error_symbol.style.display = 'grid';
                     } else {
                         error_text.textContent = 'Error: Hubo problemas consiguiendo los datos. Intente más tarde.';
+                        error_symbol.style.display = 'grid';
                     }
                     throw new Error(`Error: ${response.status}`);
                 }
@@ -27,6 +35,8 @@ function login_auth() {
             })
             .then(data => {
                 if (data.contrasena === user_password) {
+                    
+
                     const user_info = {
                         'user_id': user_id,
                         'user_role': data.rol
@@ -35,20 +45,25 @@ function login_auth() {
                     window.location.href = './home.html';
                 } else {
                     error_text.textContent = 'Contraseña incorrecta';
+                    error_symbol.style.display = 'grid';
                 }
             })
-            .catch(error =>
-                console.error('Error: ', error)
-            )
+            .catch(error => {
+                console.error('Error: ', error);
+                error_text.textContent = 'Error: Hubo problemas consiguiendo los datos. Intente más tarde.';
+                error_symbol.style.display = 'grid';
+            })
         */
         /* -> Local development */
         fetch(`http://localhost:3000/login_info/${user_id}`)
             .then(response => {
                 if (!response.ok) {
                     if (response.status === 404) {
-                        error_text.textContent = 'Error: Usuario no encontrado';
+                        error_text.textContent = 'Usuario no encontrado';
+                        error_symbol.style.display = 'grid';
                     } else {
                         error_text.textContent = 'Error: Hubo problemas consiguiendo los datos. Intente más tarde.';
+                        error_symbol.style.display = 'grid';
                     }
                     throw new Error(`Error: ${response.status}`);
                 }
@@ -64,10 +79,28 @@ function login_auth() {
                     window.location.href = './home.html';
                 } else {
                     error_text.textContent = 'Contraseña incorrecta';
+                    error_symbol.style.display = 'grid';
                 }
             })
-            .catch(error =>
-                console.error('Error: ', error)
-            )
+            .catch(error => {
+                console.error('Error: ', error);
+                error_text.textContent = 'Error: Hubo problemas consiguiendo los datos. Intente más tarde.';
+                error_symbol.style.display = 'grid';
+            })
+    }
+}
+
+function password_toggling() {
+    let passwordInput = document.getElementById("login_password");
+    let toggle_icon = document.getElementById("password_toggle");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggle_icon.classList.remove("fa-eye");
+        toggle_icon.classList.add("fa-eye-slash");
+    } else {
+        passwordInput.type = "password";
+        toggle_icon.classList.remove("fa-eye-slash");
+        toggle_icon.classList.add("fa-eye");
     }
 }

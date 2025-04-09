69134b1aa8d5cc79a7c450c7ea1b9dd4890a02c9
Ulis11 <ulig11mx@gmail.com>
2025-04-09 02:24:38 -0600

    added secure login (SECRET KEY STILL NOT FINAL)

--- public/scripts/frontend_course.js
+++ public/scripts/frontend_course.js
@@ -1,4 +1,36 @@
-let api_url = 'http://pk8ksokco8soo8ws0ks040s8.172.200.210.83.sslip.io';
+// let api_url = 'http://pk8ksokco8soo8ws0ks040s8.172.200.210.83.sslip.io';
+let api_url = 'http://localhost:3000';
+
+window.addEventListener('load', async () => {
+    let user_role;
+    let data;
+    const user_role_text = document.getElementById('nav_role');
+    try {
+        const response = await fetch(`${api_url}/user_course`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        });
+
+        if (!response.ok) {
+            if (response.status === 404) {
+                user_role_text.textContent = 'Usuario no encontrado';
+            } else if (response.status === 401) {
+                window.location.href = '/';
+            }
+            return;
+        }
+
+        data = await response.json();
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
+    user_role = data.user_role == 'alumno' ? 'Alumno' : 'Profesor';
+
+})
 
 async function log_out() {
     try {
--- public/scripts/frontend_login.js
+++ public/scripts/frontend_login.js
@@ -1,42 +1,80 @@
-function login_auth() {
-    const user_id = document.getElementById('login_user').value;
-    const user_password = document.getElementById('login_password').value;
+// let api_url = 'http://pk8ksokco8soo8ws0ks040s8.172.200.210.83.sslip.io';
+let api_url = 'http://localhost:3000';
 
-    const error_text = document.getElementById('error_message');
+async function login_auth() {
 
-    if (!user_id && !user_password) {
-        error_text.textContent = 'Error: Se requiere llenar los campos de usuario y contraseña.'
-    } else if (!user_id) {
-        error_text.textContent = 'Error: Se requiere llenar el campo de usuario.'
-    } else if (!user_password) {
-        error_text.textContent = 'Error: Se requiere llenar el campo de contraseña.'
+    const error_text = document.getElementById('error_text');
+    const error_symbol = document.getElementById('error_symbol');
+
+    const user_container = document.getElementById('login_user');
+    const password_container = document.getElementById('login_password');
+    const login_button = document.getElementById('login_button');
+    const login_button_text = document.getElementById('login_button_text');
+    const login_button_loading_wheel = document.getElementById('login_button_loading_wheel');
+    const password_toggle_wrapper = document.getElementById('icon_wrapper');
+
+    error_text.textContent = '';
+    error_symbol.display = 'none';
+
+    login_button.disabled = true;
+    login_button_text.style.display = 'none';
+    user_container.disabled = true;
+    password_container.disabled = true;
+    login_button_loading_wheel.style.display = 'inline-block';
+    password_toggle_wrapper.style.display = 'none';
+
+    try {
+        const response = await fetch(`${api_url}/login_info`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                user_id: user_container.value,
+                user_password: password_container.value
+            })
+        });
+
+        if (!response.ok) {
+            if (response.status === 404) {
+                error_text.textContent = 'Usuario no encontrado';
+            } else if (response.status === 401) {
+                error_text.textContent = 'Contraseña incorrecta';
+            } else if (response.status === 399) {
+                error_text.textContent = 'Se requiere tanto el usuario como la contraseña.';
+            } else {
+                error_text.textContent = 'Error al iniciar sesión. Intente más tarde.';
+            }
+            error_symbol.style.display = 'grid';
+            throw new Error(`Error: ${response.status}`);
+        }
+        
+        window.location.href = './home';
+
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        login_button.disabled = false;
+        login_button_text.style.display = 'inline-block';
+        user_container.disabled = false;
+        password_container.disabled = false;
+        login_button_loading_wheel.style.display = 'none';
+        password_toggle_wrapper.style.display = 'flex';
+    }
+}
+
+function password_toggling() {
+    let passwordInput = document.getElementById("login_password");
+    let toggle_icon = document.getElementById("password_toggle");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggle_icon.classList.remove("fa-eye");
+        toggle_icon.classList.add("fa-eye-slash");
     } else {
-        fetch(`http://localhost:3000/login_info/${user_id}`)
-            .then(response => {
-                if (!response.ok) {
-                    if (response.status === 404) {
-                        error_text.textContent = 'Error: Usuario no encontrado';
-                    } else {
-                        error_text.textContent = 'Error: Hubo problemas consiguiendo los datos. Intente más tarde.';
-                    }
-                    throw new Error(`Error: ${response.status}`);
-                }
-                return response.json(); 
-            })
-            .then(data => {
-                if (data.contrasena === user_password) {
-                    const user_info = {
-                        'user_id': user_id,
-                        'user_role': data.rol
-                    }
-                    sessionStorage.setItem('user_info', JSON.stringify(user_info));
-                    window.location.href = './home.html';
-                } else {
-                    error_text.textContent = 'Contraseña incorrecta';
-                }
-            })
-            .catch(error =>
-                console.error('Error: ', error)
-            )
+        passwordInput.type = "password";
+        toggle_icon.classList.remove("fa-eye-slash");
+        toggle_icon.classList.add("fa-eye");
     }
 }

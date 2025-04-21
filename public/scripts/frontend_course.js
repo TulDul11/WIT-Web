2aa14a6772f6ae56d7ed1d6ee6944529b2278ba5
Ulis11 <ulig11mx@gmail.com>
2025-04-21 17:48:52 -0600

    logout from course

--- public/scripts/frontend_course.js
+++ public/scripts/frontend_course.js
@@ -128,3 +128,21 @@ async function log_out() {
 function home_screen() {
     window.location.href = './home';
 }
+
+async function log_out() {
+    try {
+        const response = await fetch(`${api_url}/logout`, {
+            method: 'GET',
+            credentials: 'include',
+        });
+        if (!response.ok) {
+            if (response.status === 500) {
+                console.error('Cerrar sesión fallida.');
+            }
+        } else {
+            window.location.href = '/';
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}

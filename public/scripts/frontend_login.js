e1fdcc1245c7c17ce393ba4b32c1397d10c8eb03
HighTec5589Pro <ramirohector123@outlook.com>
2025-04-21 23:14:09 -0600

    actualizacion de router, app y creacion de modulos

--- public/scripts/frontend_login.js
+++ public/scripts/frontend_login.js
@@ -1,44 +1,49 @@
 function login_auth() {
     const user_id = document.getElementById('login_user').value;
     const user_password = document.getElementById('login_password').value;
-
     const error_text = document.getElementById('error_message');
 
     if (!user_id && !user_password) {
-        error_text.textContent = 'Error: Se requiere llenar los campos de usuario y contraseña.'
+        error_text.textContent = 'Error: Se requiere llenar los campos de usuario y contraseña.';
     } else if (!user_id) {
-        error_text.textContent = 'Error: Se requiere llenar el campo de usuario.'
+        error_text.textContent = 'Error: Se requiere llenar el campo de usuario.';
     } else if (!user_password) {
-        error_text.textContent = 'Error: Se requiere llenar el campo de contraseña.'
+        error_text.textContent = 'Error: Se requiere llenar el campo de contraseña.';
     } else {
-        /* -> Deployment */
-        fetch(`http://pk8ksokco8soo8ws0ks040s8.172.200.210.83.sslip.io/login_info/${user_id}`)
-            .then(response => {
-                if (!response.ok) {
-                    if (response.status === 404) {
-                        error_text.textContent = 'Error: Usuario no encontrado';
-                    } else {
-                        error_text.textContent = 'Error: Hubo problemas consiguiendo los datos. Intente más tarde.';
-                    }
-                    throw new Error(`Error: ${response.status}`);
-                }
-                return response.json(); 
+        fetch(`http://pk8ksokco8soo8ws0ks040s8.172.200.210.83.sslip.io/login_info`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                user_id: user_id,
+                user_password: user_password
             })
-            .then(data => {
-                if (data.contrasena === user_password) {
-                    const user_info = {
-                        'user_id': user_id,
-                        'user_role': data.rol
-                    }
-                    sessionStorage.setItem('user_info', JSON.stringify(user_info));
-                    window.location.href = './home.html';
-                } else {
+        })
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 404) {
+                    error_text.textContent = 'Error: Usuario no encontrado';
+                } else if (response.status === 401) {
                     error_text.textContent = 'Contraseña incorrecta';
+                } else {
+                    error_text.textContent = 'Error: Problema con el servidor. Intente más tarde.';
                 }
-            })
-            .catch(error =>
-                console.error('Error: ', error)
-            )
+                throw new Error(`Error: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const user_info = {
+                user_id: data.user_info.user_id,
+                user_role: data.user_info.user_role
+            };
+            sessionStorage.setItem('user_info', JSON.stringify(user_info));
+            window.location.href = './home.html';
+        })
+        .catch(error => {
+            console.error('Error: ', error);
+        });
         /* -> Local development */
         /*
         fetch(`http://localhost:3000/login_info/${user_id}`)
--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const db = require('../config/db');
 
@@ -17,47 +18,175 @@ router.get('/<<Extension donde quieres el fetch>><</: (Solo si requieres inputs)
 })
 */
 
-router.get('/login_info/:id', async (req, res) => {
-    try {
-        const user_id = req.params.id;
-        const [results] = await db.query('SELECT contrasena, rol FROM usuarios WHERE id = ?', [user_id]);
+router.post('/login_info', async (req, res) => {
+  try {
+      const { user_id, user_password } = req.body;
 
-        if (results.length == 0) {
-            return res.status(404).json({
-                message: 'Error: Usuario no encontrado'
-            })
-        }
+      if (!user_id || !user_password) {
+          return res.status(399).json({ message: 'Se requiere tanto el usuario como la contraseña.' });
+      }
 
-        res.json(results[0]);
-    } catch (err) {
-        res.status(500).json({
-            error: err.message
-        });
-    }
+      const [results] = await db.query(`SELECT hashing, salt, rol FROM usuarios WHERE id = ?`, [user_id]);
+
+      if (results.length == 0) {
+          return res.status(404).json({
+              message: 'Error: Usuario no encontrado'
+          })
+      } else {
+          const hashed_password = await bcrypt.hash(user_password, results[0].salt);
+          if (hashed_password != results[0].hashing) {
+              return res.status(401).json({
+                  message: 'Error: Contraseña incorrecta'
+              })
+          }
+      } 
+      const user_role = results[0].rol;
+
+      req.session.user = {user_id: user_id, user_role: user_role};
+
+      return res.json({
+          user_info: {
+              user_id: user_id,
+              user_role: user_role
+          }
+      });
+  } catch (err) {
+      res.status(500).json({
+          error: err.message
+      });
+  }
 })
 
-router.get('/user_home/:id/:role', async (req, res) => {
-    try {
-        const user_id = req.params.id;
-        const user_role = req.params.role;
+router.get('/logout', async (req, res) => {
+  try {
+      req.session.destroy((err) => {
+          if (err) {
+              return res.status(500).send('Failed to log out');
+          }
+      });
+      res.json({ message: 'Logged out successfully' });
+  } catch (err) {
+      res.status(500).json({
+          error: err.message
+      });
+  }
+});
 
-        const query = `SELECT id, nombre, apellido FROM ${user_role} WHERE id_usuario = ${user_id}`
+router.post('/user_home', async (req, res) => {
+  try {
+      if (!req.session.user) {
+          return res.status(401).json({ message: 'Sesión expirada o no iniciada.' });
+      }
 
-        const [results] = await db.query(query);
+      const user_id = req.session.user.user_id;
 
-        if (results.length == 0) {
-            return res.status(404).json({
-                message: 'Error: Usuario no encontrado'
-            })
-        }
+      const role_table = req.session.user.user_role == 'alumno' ? 'alumnos' : 'profesores';
 
-        res.json(results[0]);
-    } catch (err) {
-        res.status(500).json({
-            error: err.message
-        });
-    }
+      const query = `SELECT nombre, apellido FROM ${role_table} WHERE id_usuario = '${user_id}'`
+
+      const [results] = await db.query(query);
+
+      if (results.length == 0) {
+          return res.status(404).json({
+              message: 'Error: Usuario no encontrado'
+          })
+      }
+
+      results[0].user_id = user_id;
+      results[0].user_role = req.session.user.user_role;
+
+      return res.json(results[0]);
+  } catch (err) {
+      res.status(500).json({
+          error: err.message
+      });
+  }
 })
+
+router.post('/user_courses', async (req, res) => {
+  try {
+      if (!req.session.user) {
+          return res.status(401).json({ message: 'Sesión expirada o no iniciada.' });
+      }
+      
+      const { user_role, user_id} = req.body;
+
+      const query = `SELECT id FROM ${user_role} WHERE id_usuario = '${user_id}'`
+
+      const [current_user] = await db.query(query);
+
+      if (current_user.length == 0) {
+          return res.status(404).json({
+              message: 'Error: Usuario no encontrado'
+          })
+      }
+
+      let num_id = current_user[0].id;
+
+      let course_codes_query;
+
+      if (user_role == 'alumnos') {
+          course_codes_query = `SELECT cod_curso FROM alumnos_cursos WHERE id_alumno = ${num_id}`
+      } else if (user_role == 'profesores') {
+          course_codes_query = `SELECT cod_curso FROM profesores_cursos WHERE id_profesor = ${num_id}`
+      }
+
+      const [courses_codes] = await db.query(course_codes_query);
+
+      if (courses_codes.length == 0) {
+          return res.status(404).json({
+              message: 'No está inscrito en ningún curso.'
+          })
+      }
+
+      let course_data = []
+
+      for (let row of courses_codes) {
+          let code_course = row.cod_curso;
+          
+          let course_query = `SELECT * FROM cursos WHERE cod = '${code_course}'`;
+
+          const [course] = await db.query(course_query);
+
+          if (course.length > 0) {
+              course_data.push(course);
+          }
+      }
+
+      return res.json({course_data: course_data})
+
+  } catch (err) {
+      res.status(500).json({
+          error: err.message
+      });
+  }
+})
+
+router.post('/agregar_curso', async (req, res) => {
+  try {
+      const { cod, nombre, descripcion } = req.body;
+
+      // Verificar si los campos están completos
+      if (!cod || !nombre || !descripcion) {
+          return res.status(400).json({ message: 'Faltan campos obligatorios' });
+      }
+
+      // Verifica si el curso ya existe en la base de datos
+      const [existingCourse] = await db.query('SELECT * FROM cursos WHERE cod = ?', [cod]);
+      if (existingCourse.length > 0) {
+          return res.status(409).json({ message: 'El curso ya existe con esa clave.' });
+      }
+
+      // Agregar el curso a la base de datos
+      await db.query('INSERT INTO cursos (cod, nombre, descripcion) VALUES (?, ?, ?)', [cod, nombre, descripcion]);
+
+      // Retornar respuesta exitosa
+      res.status(201).json({ message: 'Curso agregado exitosamente' });
+  } catch (err) {
+      console.error('Error al agregar curso:', err);
+      res.status(500).json({ error: err.message });
+  }
+});
 
 //guardar contenido del modulo
 router.post('/modulos', async (req, res) => {
@@ -145,8 +274,7 @@ router.get('/modulos/:id/preguntas', async (req, res) => {
         correcta: p.correcta
       };
     });
-    
-    
+     
 
     res.json(data);
   } catch (err) {
@@ -208,7 +336,25 @@ router.get('/modulos', async (req, res) => {
     res.status(500).json({ error: 'Error interno' });
   }
 });
+router.put('/modulos/:id', async (req, res) => {
+  const moduloId = parseInt(req.params.id);
+  const { titulo, contenido } = req.body;
 
-  
+  try {
+    const [result] = await db.query(
+      'UPDATE modulos SET titulo = ?, contenido_html = ? WHERE id = ?',
+      [titulo, contenido, moduloId]
+    );
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: 'Módulo no encontrado' });
+    }
+
+    res.json({ message: 'Módulo actualizado correctamente' });
+  } catch (err) {
+    console.error("Error al actualizar módulo:", err);
+    res.status(500).json({ error: 'Error interno al actualizar' });
+  }
+});  
 
 module.exports = router;

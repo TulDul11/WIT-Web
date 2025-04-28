ef268e8cf2f14a32db69f46f48dc643f906381cf
hectorjgutierrezv@gmail.com <tuemail@ejemplo.com>
2025-04-28 17:37:34 -0600

    api

--- public/scripts/frontend_course.js
+++ public/scripts/frontend_course.js
@@ -1,5 +1,5 @@
-//let api_url = 'http://pk8ksokco8soo8ws0ks040s8.172.200.210.83.sslip.io';
-let api_url = 'http://localhost:3000';
+let api_url = 'http://pk8ksokco8soo8ws0ks040s8.172.200.210.83.sslip.io';
+
 
 window.addEventListener('load', async () => {
     // Cargamos la página, incluyendo las barras lateral y de navegación, junto con el contenido de home.
--- public/scripts/frontend_home.js
+++ public/scripts/frontend_home.js
@@ -1,5 +1,6 @@
 let api_url = 'http://pk8ksokco8soo8ws0ks040s8.172.200.210.83.sslip.io';
 
+
 /*
 Función que cargará cuando todo el contenido html y css cargé en home.html.
 */

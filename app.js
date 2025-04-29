6b93ab6e7a8fb40ffe69e818d8d1f6c7672fb758
TulDul11 <dinichen1108@gmail.com>
2025-04-29 01:20:21 -0600

    Testing some backend/frontend logic

--- app.js
+++ app.js
@@ -56,16 +56,5 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', router);
 
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
-});
-app.get('/course', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'course.html'));
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {});

6c1f46d0133a31d1ea58f951d10c87e5932663f5
TulDul11 <dinichen1108@gmail.com>
2025-04-29 00:59:28 -0600

    Added production API URL

--- public/scripts/frontend_home.js
+++ public/scripts/frontend_home.js
@@ -1,4 +1,4 @@
-let api_url = 'http://iswg4wsw8g8wkookg4gkswog.172.200.210.83.sslip.io';
+let api_url = 'http://l408cggw004w8gwgkcwos00c.172.200.210.83.sslip.io';
 
 /*
 Función que cargará cuando todo el contenido html y css cargé en home.html.

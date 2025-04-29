649c30d396d45bc3b74b2b2b5f47bde95829f7fc
TulDul11 <dinichen1108@gmail.com>
2025-04-29 15:34:31 -0600

    Testing CORS 3

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ const allowedOrigins = [
     'http://iswg4wsw8g8wkookg4gkswog.172.200.210.83.sslip.io'
 ];
 
+app.use(cors({
+    origin: 'http://fs4k48ww88csc4skkcocg00g.172.200.210.83.sslip.io',
+    credentials: true,
+}));
+
 app.options('*', cors({
     origin: 'http://fs4k48ww88csc4skkcocg00g.172.200.210.83.sslip.io',
     credentials: true,
@@ -53,4 +58,6 @@ app.get('/course', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {});
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Backend running on port ${PORT}`);
+});
